c31d5fab092a0ed09da11bf8e52ad2394c0626fe
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-08-15 11:01:19 -0300

    Update scripts.js

--- JavaScript e Nodejs/Seção 09/scripts.js
+++ JavaScript e Nodejs/Seção 09/scripts.js
@@ -5,3 +5,9 @@ console.log(document.body.childNodes[1].childNodes);
 
 console.log(document.getElementsByTagName('h1'));
 console.log(document.getElementsByTagName('p'));
+
+console.log(document.getElementById('titulo-principal'));
+
+console.log(document.getElementsByClassName('teste'));
+
+console.log(document.querySelector('#titulo-principal h1'));
